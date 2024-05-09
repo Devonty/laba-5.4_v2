109043ab7a12897b49d127c1e056cb83680cebdc
Devonty <smurf20044@mail.ru>
2024-05-09 16:36:02 +0300

    mega super fix

--- main.py
+++ main.py
@@ -20,22 +20,14 @@ class Shape:
             painter.fillRect((x + dx) * 40, (9 - y - dy) * 40, 40, 40, self.color)
 
 
-def draw_grid(painter):
-    pen = QPen()
-    pen.setColor(QColor(100, 100, 100))  # Цвет сетки
-    painter.setPen(pen)
-    for i in range(11):
-        painter.drawLine(i * 40, 0, i * 40, 400)  # Вертикальные линии
-        painter.drawLine(0, i * 40, 400, i * 40)  # Горизонтальные линии
-
-
 class Tetris(QWidget):
     def __init__(self):
         super().__init__()
         self.board = None
         self.initUI()
         self.score = 0
-        self.board = [[0 for _ in range(10)] for _ in range(10)]
+        self.board_size = 4
+        self.board = [[0 for _ in range(self.board_size)] for _ in range(self.board_size)]
         self.available_shapes = [
             Shape([(0, 0), (0, 1), (1, 0), (1, 1)]),  # квадрат2х2
             Shape([(0, 0), (0, 1), (1, 0), (1, 0)]),  # треугольник с основанием 2
@@ -49,22 +41,29 @@ class Tetris(QWidget):
         self.setWindowTitle('Tetris with Custom Rules')
         self.show()
 
+    def draw_grid(self, painter):
+        pen = QPen()
+        pen.setColor(QColor(100, 100, 100))  # Цвет сетки
+        painter.setPen(pen)
+        for i in range(self.board_size + 1):
+            painter.drawLine(i * 40, 0, i * 40, self.board_size * 40)  # Вертикальные линии
+            painter.drawLine(0, i * 40, self.board_size * 40, i * 40)  # Горизонтальные линии
 
     def paintEvent(self, event):
         painter = QPainter(self)
-        painter.fillRect(0,0,self.width(),self.height(),QColor(255,255,255))
+        painter.fillRect(0, 0, self.width(), self.height(), QColor(255, 255, 255))
         # Отрисовка фигур в областях выбора
         for i, shape in enumerate(self.available_shapes[:3]):
-            shape.draw(painter, 1 + i * 120 // 40 +i, -7)
+            shape.draw(painter, 1 + i * 120 // 40 + i, -7)
         # Отрисовка выбранной фигуры
         if self.selected_shape is not None:
             self.selected_shape.draw(painter, self.selected_shape_pos.x(), self.selected_shape_pos.y())
         # Отрисовка игрового поля
-        for i in range(10):
-            for j in range(10):
+        for i in range(self.board_size):
+            for j in range(self.board_size):
                 if self.board[i][j] == 1:
-                    painter.fillRect(j * 40, (9 - i) * 40, 40, 40, QColor(255, 0, 0))
-        draw_grid(painter)
+                    painter.fillRect(j * 40, (self.board_size - 1 - i) * 40, 40, 40, QColor(255, 0, 0))
+        self.draw_grid(painter)
         # Отображение счета
         self.draw_score(painter)
 
@@ -76,9 +75,9 @@ class Tetris(QWidget):
             # Проверка, что клик был в нижней части окна
             if y >= 560:
                 for i in range(3):
-                    if 10 + i * 120 + i*40 <= x < 130 + i * 120 + i*40 and 560 <= y < 680:
+                    if 10 + i * 120 + i * 40 <= x < 130 + i * 120 + i * 40 and 560 <= y < 680:
                         self.selected_shape = self.available_shapes[i]
-                        self.selected_shape_pos = QPoint(x // 40, 9 - y // 40)
+                        self.selected_shape_pos = QPoint(x // 40, self.board_size - 1 - y // 40)
                         self.update()
                         break
 
@@ -92,40 +91,72 @@ class Tetris(QWidget):
     def mouseReleaseEvent(self, event):
         if event.button() == Qt.LeftButton and self.selected_shape is not None:
             x = event.x() // 40
-            y = 9 - event.y() // 40
-            if 0 <= y < 10:
-                can_place = True
+            y = self.board_size - 1 - event.y() // 40
+
+            if self.can_place(x, y, self.selected_shape):
                 for dx, dy in self.selected_shape.shape_data:
-                    if not (0 <= x + dx < 10 and 0 <= y + dy < 10 and self.board[y + dy][x + dx] == 0):
-                        can_place = False
-                        break
-                if can_place:
-                    for dx, dy in self.selected_shape.shape_data:
-                        self.board[y + dy][x + dx] = 1
-                        self.score += 1  # Увеличиваем счет на 1 за каждый установленный куб
-                    self.check_and_clear_lines_columns()
-                    self.available_shapes.remove(self.selected_shape)
-                    self.selected_shape = None
-                    self.generate_new_shapes()
-                    self.update()
-                else:
-                    self.selected_shape = None
-                    self.update()
+                    self.board[y + dy][x + dx] = 1
+                    self.score += 1  # Увеличиваем счет на 1 за каждый установленный куб
+                self.check_and_clear_lines_columns()
+                self.available_shapes.remove(self.selected_shape)
+                self.generate_new_shapes()
+
+            self.selected_shape = None
+            self.update()
+
+        self.check_end()
+
+    def can_place(self, x, y, shape: Shape) -> bool:
+        if not (0 <= y < self.board_size):
+            return False
+
+        for dx, dy in shape.shape_data:
+            if not (0 <= x + dx < self.board_size and 0 <= y + dy < self.board_size and self.board[y + dy][
+                x + dx] == 0):
+                return False
+
+        return True
+
+    def check_end(self):
+        for shape in self.available_shapes:
+            for i in range(self.board_size):
+                for j in range(self.board_size):
+                    if self.can_place(i, j, shape):
+                        return False
+        self.end_game()
+
+    def end_game(self):
+        QMessageBox.about(self, "Конец игры!", f"Ваш счёт: {self.score}")
+        self.restart()
+
+    def restart(self):
+        self.board = [[0 for _ in range(self.board_size)] for _ in range(self.board_size)]
+        self.available_shapes.clear()
+        self.selected_shape = None
+        self.selected_shape_pos = None
+        self.score = 0
+
+        self.generate_new_shapes()
 
     def generate_new_shapes(self):
-        if self.selected_shape is None:  # Проверка наличия выбранной фигуры
-            new_shapes = [
-                Shape([(0, 0), (0, 1), (1, 0), (1, 1)]),  # квадрат2х2
-                Shape([(0, 0), (0, 1), (1, 0), (1, 0)]),  # треугольник с основанием 2
-                Shape([(0, 0), (0, 1), (0, 2)]),  # вертикальная палка на 3
-                Shape([(0, 0), (1, 0), (2, 0), (2, 1)]),  # треугольник с основанием 3
-                Shape([(0, 0), (1, 0), (2, 0), (1, 1)]),  # перевернутая т
-                Shape([(0, 0), (1, 0), (1, 1), (2, 1)]),  # z
-                Shape([(0, 0), (0, 1), (0, 2), (0, 3)]),  # вертикальная палка на 4
-                Shape([(0, 0), (0, 1), (0, 2), (1, 1)]),  # перевернутая т
-                Shape([(0, 0)])  # точка
-            ]
-            self.available_shapes.append(random.choice(new_shapes))
+        # Проверка наличия выбранной фигуры
+        if len(self.available_shapes) >= 3:
+            return
+
+        new_shapes = [
+            Shape([(0, 0), (0, 1), (1, 0), (1, 1)]),  # квадрат2х2
+            Shape([(0, 0), (0, 1), (1, 0), (1, 0)]),  # треугольник с основанием 2
+            Shape([(0, 0), (0, 1), (0, 2)]),  # вертикальная палка на 3
+            Shape([(0, 0), (1, 0), (2, 0), (2, 1)]),  # треугольник с основанием 3
+            Shape([(0, 0), (1, 0), (2, 0), (1, 1)]),  # перевернутая т
+            Shape([(0, 0), (1, 0), (1, 1), (2, 1)]),  # z
+            Shape([(0, 0), (0, 1), (0, 2), (0, 3)]),  # вертикальная палка на 4
+            Shape([(0, 0), (0, 1), (0, 2), (1, 1)]),  # перевернутая т
+            Shape([(0, 0)])  # точка
+        ]
+        random.shuffle(new_shapes)
+        while len(self.available_shapes) < 3:
+            self.available_shapes.append(new_shapes.pop())
 
     def draw_score(self, painter):
         font = painter.font()
@@ -136,17 +167,25 @@ class Tetris(QWidget):
     def check_and_clear_lines_columns(self):
         lines_cleared = 0
         columns_cleared = 0
-        for i in range(10):
-            if all(cell == 1 for cell in self.board[i]):
+        for i in range(self.board_size):
+            if all(cell != 0 for cell in self.board[i]):
                 lines_cleared += 1
-                for j in range(10):
-                    self.board[i][j] = 0
-        for j in range(10):
-            if all(self.board[i][j] == 1 for i in range(10)):
+                for j in range(self.board_size):
+                    self.board[i][j] = -1
+
+        for j in range(self.board_size):
+            if all(self.board[i][j] != 0 for i in range(self.board_size)):
                 columns_cleared += 1
-                for i in range(10):
+                for i in range(self.board_size):
+                    self.board[i][j] = -1
+
+        for i in range(self.board_size):
+            for j in range(self.board_size):
+                if self.board[i][j] == -1:
                     self.board[i][j] = 0
-        self.score += (lines_cleared + columns_cleared) * 10  # Увеличиваем счет на 10 баллов за каждую удаленную строку или столбец
+
+        self.score += (
+                                  lines_cleared + columns_cleared) * self.board_size  # Увеличиваем счет на 10 баллов за каждую удаленную строку или столбец
         self.update()
 
 
